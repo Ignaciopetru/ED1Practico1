995780323a672221eb59e5139041d190f9f98086
Lucio Trincheri <lucio4852071@gmail.com>
2020-04-20 18:11:18 -0300

    Primer commit

---
+++ comparacion.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "comparacion.h"
+
+int comparacion_edad(Persona *persona1, Persona *persona2)
+{
+  if (persona1->edad <= persona2->edad)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }
+}
+
+int comparacion_nombre(Persona *persona1, Persona *persona2)
+{
+  if (strcmp(persona1->nombre, persona2->nombre) <= 0)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }
+}
