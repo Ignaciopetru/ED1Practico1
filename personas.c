5b83fa0be092594730b48ad6c5bfb423343ba4a4
Lucio Trincheri <lucio4852071@gmail.com>
2020-04-21 15:13:25 -0300

    Finalizo limpieza de codigo

--- personas.c
+++ personas.c
@@ -4,25 +4,20 @@
 #include <malloc.h>
 #include <stdbool.h>
 #include <time.h>
-//#include <locale.h>
 #include "personas.h"
 
-arrayDatos *parser(const char *filename)
-{
+Datos *parser(const char *file){
   int total_size = 200;
-  arrayDatos *lista = (arrayDatos *)malloc(sizeof(arrayDatos));
+  Datos *lista = (Datos *)malloc(sizeof(Datos));
   char **array = (char **)malloc(sizeof(char *) * total_size);
   FILE *fp;
-  fp = fopen(filename, "r");
+  fp = fopen(file, "r");
   int linea;
-  for (linea = 0; !feof(fp); linea++)
-  {
+  for (linea = 0; !feof(fp); linea++){
     char *buff = malloc(sizeof(char) * 256);
     // Read a line
-    if (fscanf(fp, "%255[^\n]\n", buff) != EOF)
-    {
-      if (total_size == linea)
-      {
+    if (fscanf(fp, "%255[^\n]\n", buff) != EOF){
+      if (total_size == linea){
         total_size *= 2;
         array = (char **)realloc(array, sizeof(char *) * total_size);
       }
@@ -36,41 +31,38 @@ arrayDatos *parser(const char *filename)
   return lista;
 }
 
-void escrituraSalida(arrayDatos *arrayNacimiento, arrayDatos *arrayPersonas, const char *filename, long cant)
-{
+void escritura(FILE *fp, char* first, int second, char* third){
+  fprintf(fp, "%s, %d, %s\n", first, second, third);
+}
+
+void escSalida(Datos *datosNac, Datos *datosPer, const char *file, long cant){
   srand(time(NULL));
   FILE *fp;
-  // Abrimios un archivo en modo escritura, sino existe se creara.
-  fp = fopen(filename, "w");
+  fp = fopen(file, "w");
   int i;
-  for (i = 0; i < cant; i++)
-  {
-    int randPersona = rand() % arrayPersonas->largo;
-    int randEdad = (rand() % 100) + 1;
-    int randNacimiento = rand() % arrayNacimiento->largo;
-    fprintf(fp, "%s, %d, %s\n", arrayPersonas->array[randPersona], randEdad, arrayNacimiento->array[randNacimiento]);
+  for (i = 0; i < cant; i++){
+    int ranPer = rand() % datosPer->largo;
+    int ranEdad = (rand() % 100) + 1;
+    int ranNac = rand() % datosNac->largo;
+    escritura(fp, datosPer->array[ranPer], ranEdad, datosNac->array[ranNac]);
   }
   fclose(fp);
 }
 
-void freeArrays(arrayDatos *array)
-{
+void freeDatos(Datos *array){
   int i;
-  for (i = 0; i < array->largo; i++)
-  {
+  for (i = 0; i < array->largo; i++){
     free(array->array[i]);
   }
-  free(array->largo);
   free(array);
 }
 
-int main(int argc, char *argv[])
-{
-  arrayDatos *arrayNacimiento = parser("datos/paises.txt");
-  arrayDatos *arrayPersonas = parser("datos/nombres1.txt");
+int main(int argc, char *argv[]){
+  Datos *datosNac = parser("datos/paises.txt");
+  Datos *datosPer = parser("datos/nombres1.txt");
   long cant = strtol(argv[1], NULL, 10);
-  escrituraSalida(arrayNacimiento, arrayPersonas, "salida.txt", cant);
-  freeArrays(arrayNacimiento);
-  freeArrays(arrayPersonas);
+  escSalida(datosNac, datosPer, "salida.txt", cant);
+  freeDatos(datosNac);
+  freeDatos(datosPer);
   return 0;
 }
